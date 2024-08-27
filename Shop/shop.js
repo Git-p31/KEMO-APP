aa5747ead4dcae7ae594a4890cf1a2194da84d9c
Git-p31 <lolpoking18@gmail.com>
2024-08-27 18:31:58 +0300

    Kemo-app creat

---
+++ Shop/shop.js
@@ -0,0 +1,8 @@
+function toggleSideMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    if (sideMenu.style.width === '250px') {
+        sideMenu.style.width = '0';
+    } else {
+        sideMenu.style.width = '250px';
+    }
+}
