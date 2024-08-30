a6443dc8c4adc93c7c5bdba954af9cd2d4783f24
Git-p31 <lolpoking18@gmail.com>
2024-08-30 10:45:45 +0300

    test

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSideMenu() {
-    const sideMenu = document.getElementById('sideMenu');
-    if (sideMenu.style.width === '250px') {
-        sideMenu.style.width = '0';
-    } else {
-        sideMenu.style.width = '250px';
-    }
-}
